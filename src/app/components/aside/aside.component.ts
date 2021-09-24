bcba40320ff17ccc7fcec8a4f1df84f8fccbbe7c
Allan ROLLE <allan.rolle@laposte.net>
2021-09-24 12:28:04 +0200

    add functionnality brand selection

--- src/app/components/aside/aside.component.ts
+++ src/app/components/aside/aside.component.ts
@@ -8,10 +8,25 @@ import { ProductsService } from 'src/app/services/products.service';
 })
 export class AsideComponent implements OnInit {
 
+  count:any = [];
+  selection:any[] = [];
+
   constructor(private ProductsService: ProductsService) { }
 
   ngOnInit(): void {
-    console.log(this.ProductsService.getBrands())
+    this.count = this.ProductsService.getCount();
+  }
+  handleBrandSelection($event:any) {
+    let currentValue = $event.target.value;
+    if(this.selection.indexOf(currentValue) === -1) {
+      this.selection.push(currentValue)
+    }else{
+      this.selection = this.selection.filter(item => item !== currentValue)
+    }
+  }
+  submitBrandSelection($event:any){
+    $event.preventDefault();
+    console.log(this.selection)
   }
 
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,22 +1,19 @@
 import { Injectable } from '@angular/core';
-import { productInfo } from '../../assets/data';
 import  data  from '../../assets/data';
-
-type obj = {
-  name: string,
-  count: number
-}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+  count: any = {};
 
   constructor() {}
 
-  getBrands(){    
-
-      return data.map(el => el.brand).filter((el,i,arr) => arr.indexOf(el) === i);
+  getCount():object{
+    data.forEach(el => {
+      this.count[el.brand] = (this.count[el.brand] || 0) + 1
+    });
+    return this.count;
   }
 
 }
