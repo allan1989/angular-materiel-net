945b64ad7230425f130821e8c9a3d24a13752ade
Allan ROLLE <allan.rolle@laposte.net>
2021-10-03 12:46:15 +0200

    change to BehaviorSubject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from './services/products.service';
 import { Subscription } from 'rxjs';
+import { productInfo } from 'src/assets/data';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,7 @@ import { Subscription } from 'rxjs';
 })
 export class AppComponent implements OnInit{
   public title = 'toto'; 
-  public products: any[] = [];
+  public products: productInfo[] = [];
   public subscription: Subscription;
   constructor(private ProductsService: ProductsService){
     this.subscription = this.ProductsService.getProductsObs().subscribe(el => this.products = el)
@@ -19,4 +20,22 @@ export class AppComponent implements OnInit{
     this.products = this.ProductsService.getProducts();
   }
 
+  onSort($event:any):productInfo[] {
+    switch($event.target.value) {
+      case 'brand' :
+        return this.ProductsService.subject.value.sort((a,b) => {
+          if(a.brand.toLowerCase() > b.brand.toLowerCase()) return 1;
+          if(a.brand.toLowerCase() < b.brand.toLowerCase()) return -1;
+          return 0
+        })
+        case 'asc':
+          return this.ProductsService.subject.value.sort((a,b) => a.price - b.price)
+        case 'desc':
+          return this.ProductsService.subject.value.sort((a,b) => b.price - a.price)
+        case 'reviews':
+          return this.ProductsService.subject.value.sort((a,b) => b.customerReview - a.customerReview)  
+        default:
+          return this.products 
+    }
+  }
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import  data  from '../../assets/data';
-import { Observable, Subject } from 'rxjs';
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
 import { productInfo } from '../../assets/data';
 
 @Injectable({
@@ -8,36 +8,36 @@ import { productInfo } from '../../assets/data';
 })
 export class ProductsService {
   count: any = {};
-  subject = new Subject<productInfo[]>();
-  data :any[] = [];
+  subject = new BehaviorSubject<productInfo[]>(data);
+  data: productInfo[] = [];
 
   constructor() {}
 
   getCount():object{
-    data.forEach(el => {
+    this.subject.value.forEach(el => {
       this.count[el.brand] = (this.count[el.brand] || 0) + 1
     });
     return this.count;
   }
 
   getProducts():productInfo[]{
-    this.subject.subscribe(data => this.data = data);
+    //this.subject.subscribe(data => this.data = data);
     this.subject.next(data);
-    return this.data;
+    return this.subject.value;
   }
 
   filter(brands:any):productInfo[]{
     if(brands.length === 0 ) {
       return this.getProducts();
     }
-    this.subject.subscribe(data => this.data = data);
+    //this.subject.subscribe(data => this.data = data);
     this.data = data.filter(el => brands.indexOf(el.brand) > -1);
     this.subject.next(this.data);
-    return this.data;
+    return this.subject.value;
   }
 
   getProductsObs(): Observable<any> {
     return this.subject.asObservable();
-}
+  }
 
 }
